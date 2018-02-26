7285579948284063013d8a6eb610ef4808d568e5
jude juma <judejuma@gmail.com>
2018-02-26 13:02:12 +0300

    Adding codes on the user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//user
+//user interface
 $(document).ready(function() {
   $("form#pingPongForm").submit(function(event) {
     $("#userOutput").empty();
@@ -10,21 +10,3 @@ $(document).ready(function() {
   });
 });
 });
-
-//business
-function pingPongFun(userInput){
-var result = [];
-for (var i = 1; i <= userInput; i++){
-  if ((i % 15) === 0){
-    result.push ("Ping-Pong");
-  } else if ((i % 5) === 0){
-    result.push ("Pong");
-
-  } else if ((i % 3) === 0){
-    result.push ("Ping");
-
-  } else
-    result.push (i);
-}
-return result;
-}
