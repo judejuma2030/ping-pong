9b495cf4339fe8e0dc7b2bc9db232c50e1959b1d
jude juma <judejuma@gmail.com>
2018-02-26 13:03:56 +0300

    Adds the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,19 @@ $(document).ready(function() {
   });
 });
 });
+//business logic
+function pingPongFun(userInput){
+var result = [];
+for (var i = 1; i <= userInput; i++){
+  if ((i % 15) === 0){
+    result.push ("Ping-Pong");
+  } else if ((i % 5) === 0){
+    result.push ("Pong");
+
+  } else if ((i % 3) === 0){
+    result.push ("Ping");
+  } else
+    result.push (i);
+}
+return result;
+}
